802c391cf84f47df49b7afe8a8ec1c20b5c5ec4a
princeofkondur <princeofkondur@gmail.com>
2026-09-03 16:51:53 +0530

    Add calculator program and modified hello cpp

---
+++ App/Calculator.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int add(int , int);
+
+int add(int x, int y)
+{
+	return x + y;
+}
+
--- App/Main.cpp
+++ App/Main.cpp
@@ -4,6 +4,9 @@ using namespace std;
 int main()
 {
 	cout << "Hi Welcome to the world of GitHub Actions in C++\n";
+	int a, b = 0;
+
+	int c = add(a + b);
 
 	return 0;
 }
